4c09108beef3788ebda0417721704bad0a00ca86
zfrkrc <zfrkrc@gmail.com>
2026-02-24 12:22:53 +0300

    Düzeltme

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,26 +68,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="tr">
+      <head>
+        <>
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-JT8FSSMHLK"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-JT8FSSMHLK');
+            `}
+          </Script>
+        </>
+      </head>
       <body className="antialiased">
         <Navbar />
         {children}
         <Footer />
-        {process.env.NEXT_PUBLIC_GA_ID && (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-              strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-              `}
-            </Script>
-          </>
-        )}
       </body>
     </html>
   );
