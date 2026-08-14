abe7301f0bf99311f5affe490ae432791af28831
zafer <zfrkrc@gmail.com>
2026-08-14 05:08:41 +0300

    fix: GTM direct load (revert first-party /sbgc proxy, Cloudflare 404)

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,13 +69,13 @@ export default function RootLayout({
   return (
     <html lang="tr">
       <head>
-        {/* Google Tag Manager - First-Party Proxy via Cloudflare Gateway */}
+        {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'/sbgc?id='+i+dl;f.parentNode.insertBefore(j,f);
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-M2S927G');`,
           }}
         />
@@ -88,10 +88,10 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         />
       </head>
       <body className="antialiased">
-        {/* Google Tag Manager (noscript) - Proxy via Cloudflare Gateway */}
+        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="/ns.html?id=GTM-M2S927G"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M2S927G"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
